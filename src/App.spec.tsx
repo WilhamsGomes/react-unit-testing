6584e103cedbd071e1b751794ec75771a22cf7dd
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-04-06 12:33:18 -0300

    first commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,27 @@
+import { render, fireEvent } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import App from "./App"
+
+// test('sum', () => {
+
+//     const { getByText } =  render(<App />)
+
+//     expect(getByText('Hello World')).toBeInTheDocument()
+// })
+
+describe('App Component', () => {
+    it('should render list items', () => {
+        const { getByText } = render(<App />)
+        expect(getByText('Diego')).toBeInTheDocument()
+        expect(getByText('Rodz')).toBeInTheDocument()
+        expect(getByText('Mayk')).toBeInTheDocument()
+    });
+
+    it('should be able to add new item to the list', () => {
+        const { getByText } = render(<App />)
+        const addButton = getByText('Adicionar');
+        fireEvent.click(addButton)
+
+        expect(getByText('Novo')).toBeInTheDocument()
+    });
+});
